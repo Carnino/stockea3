024e7d65b65d9799e5211f7228906dc8baf7bc26
ulisescerq <cerquattiulises@gmail.com>
2025-04-03 19:50:13 -0300

    atributos entidad movimiento

--- src/movimiento/entities/movimiento.entity.ts
+++ src/movimiento/entities/movimiento.entity.ts
@@ -1,1 +1,19 @@
-export class Movimiento {}
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+
+@Entity('movimiento')
+@Unique(['id'])
+
+export class Movimiento {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'timestamptz'})
+    fechaHora: string;
+    
+    @Column({ type: 'text'})
+    tipoMovimiento: string;
+    
+    @Column({ type: 'integer', nullable: false })
+    total: number;
+}
