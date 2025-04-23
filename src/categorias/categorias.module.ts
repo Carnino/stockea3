d6e74cca54b3d3220a4b7eda5286b0d2ed3f8c73
ulisescerq <cerquattiulises@gmail.com>
2025-04-23 11:07:26 -0300

    correccion categoria

--- src/categorias/categorias.module.ts
+++ src/categorias/categorias.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriasController } from './categorias.controller';
+import { Categoria } from './entities/categoria.entity';
 
 @Module({
   controllers: [CategoriasController],
   providers: [CategoriasService],
+  imports: [
+      TypeOrmModule.forFeature([Categoria]),
+    ],
 })
 export class CategoriasModule {}
