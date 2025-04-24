084d4cdbe1a52d4516e5558850f374be28a1b7c1
ulisescerq <cerquattiulises@gmail.com>
2025-04-24 19:30:54 -0300

    movimiento salida terminado

--- src/movimiento-salida/entities/movimiento-salida.entity.ts
+++ src/movimiento-salida/entities/movimiento-salida.entity.ts
@@ -1,1 +1,15 @@
-export class MovimientoSalida {}
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+
+@Entity('MovimientoSalida')
+    @Unique(['id'])
+    
+export class MovimientoSalida {
+    @PrimaryGeneratedColumn()
+        id: number;
+    
+        @Column({ type: 'varchar', length: 255 })
+        nombre: string;
+        
+        @Column({ type: 'text'})
+        descripcion: string;
+}
