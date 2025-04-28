6ab09ac2076914090cb26421fe5095ebd42d9704
ulisescerq <cerquattiulises@gmail.com>
2025-04-28 12:54:43 -0300

    movimiento salida terminado

--- src/movimiento-salida/movimiento-salida.service.ts
+++ src/movimiento-salida/movimiento-salida.service.ts
@@ -3,7 +3,7 @@ import { CreateMovimientoSalidaDto } from './dto/create-movimiento-salida.dto';
 import { UpdateMovimientoSalidaDto } from './dto/update-movimiento-salida.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MovimientoSalida } from './entities/movimiento-salida.entity';
-import { Repository } from 'typeorm';
+import { IsNull, Not, Repository } from 'typeorm';
 
 @Injectable()
 export class MovimientoSalidaService {
@@ -47,5 +47,27 @@ export class MovimientoSalidaService {
     const movSalida = await this.findOne(id)
 
     const removeCategoria = this.movimeintoSalidaRepository.remove(movSalida)
+  }
+  async softDelete(id: number) : Promise<void>{
+    const result =  await this.movimeintoSalidaRepository.softDelete(id)
+    if(result.affected === 0){
+      throw new NotFoundException(`El moviendo de salida con ID ${id} no se encontró`)
+    }
+  }
+
+  async restore(id:number) : Promise<void>{
+    const result = await this.movimeintoSalidaRepository.restore(id)
+    if(result.affected === 0){
+      throw new NotFoundException(`El moviendo de salida con ID ${id} no se encontró`)
+    }
+  }
+
+  async findSoftDeleted() : Promise<MovimientoSalida[]>{
+    return await this.movimeintoSalidaRepository.find({
+      where: {
+        deletedAt: Not(IsNull()),
+      },
+      withDeleted: true
+    })
   }
 }
