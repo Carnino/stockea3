7ff09559ea0a038dc90a438694ff6fe4f79b9941
ulisescerq <cerquattiulises@gmail.com>
2025-04-30 17:10:13 -0300

    validaciones de proveedor, categoria, movimiento salida

--- src/movimiento-salida/dto/update-movimiento-salida.dto.ts
+++ src/movimiento-salida/dto/update-movimiento-salida.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateMovimientoSalidaDto } from './create-movimiento-salida.dto';
+import { IsNumber, IsOptional, IsPositive, IsString } from 'class-validator';
 
-export class UpdateMovimientoSalidaDto extends PartialType(CreateMovimientoSalidaDto) {}
+export class UpdateMovimientoSalidaDto extends PartialType(CreateMovimientoSalidaDto) {
+    @IsPositive()
+    @IsNumber()
+    id: number;
+    
+    @IsString()
+    @IsOptional()
+    nombre?: string;
+
+    @IsString()
+    @IsOptional()
+    descripcion?: string;
+}
