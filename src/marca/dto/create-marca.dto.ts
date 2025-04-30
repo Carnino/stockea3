fbe037bf22651d64055c6612e7baab12777f4648
HAriass <hernanarias.utn@gmail.com>
2025-04-30 17:28:07 -0300

    validaciones producto, marca. Ver tema de crear y actualizar marca.

--- src/marca/dto/create-marca.dto.ts
+++ src/marca/dto/create-marca.dto.ts
@@ -1,5 +1,12 @@
-import { IsNotEmpty, IsString, IsNumber, Min, IsOptional } from 'class-validator';
+import { IsEmpty, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateMarcaDto {
+
+    @IsNotEmpty()
+    @IsString()
+    nombre: string
     
+    @IsOptional()
+    @IsString()
+    descripcion?: string
 }
--- src/productos/dto/update-producto.dto.ts
+++ src/productos/dto/update-producto.dto.ts
@@ -1,4 +1,34 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductoDto } from './create-producto.dto';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateProductoDto extends PartialType(CreateProductoDto) {}
+export class UpdateProductoDto extends PartialType(CreateProductoDto) {
+
+    @IsNotEmpty()
+    @IsNumber()
+    id:number
+
+    @IsString()
+    @IsOptional()
+    nombre?: string;
+
+    @IsString()
+    @IsOptional()
+    codigo?: string;
+
+    @IsString()
+    @IsOptional()
+    descripcion?: string;
+/* 
+    @IsNumber()
+    @IsOptional()
+    categoria?: number;
+
+    @IsNumber()
+    @IsOptional()
+    marca?: number;
+
+    @IsNumber()
+    @IsOptional()
+    proveedor?: number; */
+}
