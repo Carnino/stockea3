c1362cdd12bd47cc39b8fc5c38f5bceeb9ffc446
HAriass <hernanarias.utn@gmail.com>
2025-05-15 12:19:57 -0300

    Cambio logica stock

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MarcaModule } from './marca/marca.module';
 import { ProductosEnStockModule } from './productos-en-stock/productos-en-stock.module';
 import { MovimientoSalidaModule } from './movimiento-salida/movimiento-salida.module';
+import { MovimientoModule } from './movimiento/movimiento.module';
+import { StockModule } from './stock/stock.module';
 
 @Module({
   imports: [TypeOrmModule .forRoot({
@@ -20,7 +22,7 @@ import { MovimientoSalidaModule } from './movimiento-salida/movimiento-salida.mo
     entities: [__dirname+'/**/*.entity.{ts,js}'],
     synchronize: true, //SE USA SOLO EN DESARROLLO
     dropSchema: false
-  }),ProductosModule, ProveedorModule, CategoriasModule, MarcaModule, ProductosEnStockModule, MovimientoSalidaModule],
+  }),ProductosModule, ProveedorModule, CategoriasModule, MarcaModule, ProductosEnStockModule, MovimientoSalidaModule, MovimientoModule, StockModule],
   controllers: [AppController],
   providers: [AppService],
 })
