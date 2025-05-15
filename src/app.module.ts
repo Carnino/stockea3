c7a8f08752a0038f0809c5144d3a367c087b4cc7
HAriass <hernanarias.utn@gmail.com>
2025-05-15 15:37:15 -0300

    stock

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { ProveedorModule } from './proveedores/proveedor.module';
 import { CategoriasModule } from './categorias/categorias.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MarcaModule } from './marca/marca.module';
-import { ProductosEnStockModule } from './productos-en-stock/productos-en-stock.module';
-import { MovimientoSalidaModule } from './movimiento-salida/movimiento-salida.module';
 import { MovimientoModule } from './movimiento/movimiento.module';
 import { StockModule } from './stock/stock.module';
 
@@ -22,7 +20,7 @@ import { StockModule } from './stock/stock.module';
     entities: [__dirname+'/**/*.entity.{ts,js}'],
     synchronize: true, //SE USA SOLO EN DESARROLLO
     dropSchema: false
-  }),ProductosModule, ProveedorModule, CategoriasModule, MarcaModule, ProductosEnStockModule, MovimientoSalidaModule, MovimientoModule, StockModule],
+  }),ProductosModule, ProveedorModule, CategoriasModule, MarcaModule, MovimientoModule, StockModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/movimiento/entities/movimiento.entity.ts
+++ src/movimiento/entities/movimiento.entity.ts
@@ -1,1 +1,29 @@
-export class Movimiento {}
+import { Producto } from "src/productos/entities/producto.entity";
+import { Binary, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('movimiento')
+export class Movimiento {
+
+    @PrimaryGeneratedColumn()
+    codigo: number;
+
+    @Column({ type: 'varchar', length: 255 })
+    nombre: string;
+
+    @Column({ type:'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    fecha:Date;
+
+    @Column( { type:'double precision' } ) 
+    costo:number
+
+    @ManyToOne(() => Producto)
+    @JoinColumn({ name: 'producto_id' })
+    producto:Producto
+
+    @Column({ type: 'int'})
+    cantidad:number
+
+    @Column({type: 'bytea'})
+    tipoMovimiento: Binary
+    
+}
