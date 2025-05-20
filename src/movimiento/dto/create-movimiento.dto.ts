af615ec9d044b245b61aa048f3fb8e2be811193e
HAriass <hernanarias.utn@gmail.com>
2025-05-20 17:51:17 -0300

    DTO movimiento terminado

--- src/movimiento/dto/create-movimiento.dto.ts
+++ src/movimiento/dto/create-movimiento.dto.ts
@@ -1,1 +1,25 @@
-export class CreateMovimientoDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, Min } from "class-validator";
+
+export class CreateMovimientoDto {
+
+    @IsNotEmpty()
+    @IsString()
+    nombre: string;
+
+    @IsOptional()
+    @IsNumber()
+    @Min(0, { message: 'El precio no puede ser negativo' })
+    costo?:number
+
+    @IsNumber()
+    @IsNotEmpty()
+    producto:number
+
+    @IsNumber()
+    @IsPositive({ message: 'La cantidad debe ser positiva' })
+    @Min(1, { message: 'La cantidad debe ser como minimo 1' })
+    cantidad:number
+
+    @IsNumber()
+    tipoMovimiento: number 
+}
