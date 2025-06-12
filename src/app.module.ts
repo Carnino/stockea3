282f745dd205878c1c6ce1c4f8a1089d80cb05aa
martin-coser <cosermartin@hotmail.com>
2025-06-12 19:06:48 -0300

    corrreccionesDeploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,15 +10,23 @@ import { MovimientoModule } from './movimiento/movimiento.module';
 
 @Module({
   imports: [TypeOrmModule .forRoot({
-    type: 'postgres',
+/*     type: 'postgres',
     host: 'localhost',
     port: 5432,
     username: 'postgres',
     password: 'postgres',
-    database: 'stockea3',
-    entities: [__dirname+'/**/*.entity.{ts,js}'],
-    synchronize: true, //SE USA SOLO EN DESARROLLO
-    dropSchema: false
+    database: 'stockea3', */
+    //entities: [__dirname+'/**/*.entity.{ts,js}'],
+    //synchronize: true, //SE USA SOLO EN DESARROLLO
+    //dropSchema: false
+    type: 'postgres',
+    host: process.env.PGHOST,
+    port: parseInt(process.env.PGPORT || '5432', 10),
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: true,
   }),ProductosModule, ProveedorModule, CategoriasModule, MarcaModule, MovimientoModule],
   controllers: [AppController],
   providers: [AppService],
