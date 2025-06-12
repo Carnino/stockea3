555b27ab2b46993bc630c692a024c60061169dd8
martin-coser <cosermartin@hotmail.com>
2025-06-12 19:36:07 -0300

    fe

--- src/main.ts
+++ src/main.ts
@@ -2,58 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as pgtools from 'pgtools';
 
 async function bootstrap() {
-  // Configurar las variables de entorno
-  const PORT = process.env.PORT || 4000; // Usar el puerto de entorno o 4000 si no está definido
-  const configdb = {
-    user: process.env.PGUSER || 'postgres',
-    host: process.env.PGHOST || 'localhost',
-    password: process.env.PGPASSWORD || 'postgres',
-    port: parseInt(process.env.PGPORT || '5432', 10),
-    database: process.env.PGDATABASE || 'stockea3',
-  };
+  const PORT = process.env.PORT || 4000;
 
-  try {
-    // Intenta crear la base de datos antes de iniciar la aplicación
-    //await pgtools.createdb(configdb, configdb.database);
-    console.log(`✅ Base de datos '${configdb.database}' creada exitosamente.`);
-  } catch (err) {
-    if (err.name === 'duplicate_database') {
-      console.log(`⚠️ La base de datos '${configdb.database}' ya existe.`);
-    } else {
-      console.error('❌ Error al crear la base de datos:', err);
-      process.exit(1); // Detener la aplicación si no se puede crear la DB
-    }
-  }
-
-  // Iniciar la aplicación NestJS
   const app = await NestFactory.create(AppModule);
 
-  // Configurar Swagger para la documentación de la API
+  // Configurar Swagger
   const config = new DocumentBuilder()
     .setTitle('📦 Stockea3 - API')
-    .setDescription(
-      'Documentación de la API de Stockea3, incluyendo endpoints, parámetros y respuestas.',
-    )
+    .setDescription('Documentación de la API de Stockea3.')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  // Habilitar validaciones globales
+  // Validaciones globales
   app.useGlobalPipes(new ValidationPipe());
 
-  // Habilitar CORS global
+  // CORS
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
-  // Escuchar en el puerto definido por las variables de entorno
-  await app.listen(PORT, () => {
+  // Escuchar en puerto definido
+  await app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
   });
 }
